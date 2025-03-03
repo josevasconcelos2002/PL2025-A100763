69085fcf914a13c11a9fd66465d1f5942d4bb2db
Jose Vasconcelos <zevasconcelos6@gmail.com>
2025-03-03 09:41:05 +0000

    Inicio TPC4

---
+++ TPC4/README.md
@@ -0,0 +1,15 @@
+# TPC4
+
+Data: 03/03/2025 <br>
+Nome: José Pedro Torres Vasconcelos <br>
+Número Mecanográfico: A100763 <br> <br> <br>
+
+![José Vasconcelos, A100763](/images/me.png)
+
+
+<br>
+
+## **Resumo**
+
+## **Resultados**
+
---
+++ TPC4/tpc4.py
@@ -0,0 +1,16 @@
+import sys
+import re
+
+
+
+
+def main(argc, argv):
+    if(argc < 2):
+        print("USAGE: tpc4.py <file>")
+    
+    elif(argc == 2):
+        file = argv[1]
+    
+    
+if __name__ == '__main__':
+    main(len(sys.argv), sys.argv)
