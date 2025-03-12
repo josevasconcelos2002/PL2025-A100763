846eae192ababfe107d2408f93cf27cf4b0fa687
Jose Vasconcelos <zevasconcelos6@gmail.com>
2025-03-12 08:39:37 +0000

    TPC5 quase pronto

--- TPC5/Maquina_Vending.py
+++ TPC5/Maquina_Vending.py
@@ -32,3 +32,29 @@ class Maquina_Vending:
     
     def ver_stock(self):
         print(self.stock)
+    
+    def listar(self):
+        print("maq:")
+        print("cod  | nome              | quantidade | preço")
+        print("--------------------------------------------")
+        for item in self.stock:
+            print(f"{item['cod']:4} | {item['nome']:18}| {item['quant']:10} | {item['preco']:>5.2f}€")
+            
+    def devolver_troco(self):
+        troco_disponivel = {
+            200: "2e", 100: "1e",
+            50: "50c", 20: "20c",
+            10: "10c", 5: "5c",
+            2: "2c", 1: "1c"
+        }
+        troco = int(self.saldo * 100)
+        resultado_troco = {}
+
+        for valor, moeda in troco_disponivel.items():
+            if troco >= valor:
+                quantidade = troco // valor
+                troco -= quantidade * valor
+                resultado_troco[moeda] = quantidade
+
+        self.saldo = 0.0
+        return ", ".join(f"{qtd}x {moeda}" for moeda, qtd in resultado_troco.items())
--- TPC5/tpc5.py
+++ TPC5/tpc5.py
@@ -1,6 +1,7 @@
 import sys
 from Maquina_Vending import Maquina_Vending
 import json
+from datetime import datetime
 
 def start(stock_file):
         
@@ -12,7 +13,36 @@ def start(stock_file):
         
     maquina = Maquina_Vending()
     maquina.set_stock(stock)
-    maquina.ver_stock()
+    #maquina.ver_stock()
+    if maquina.stock:
+        data = datetime.today().strftime("%Y-%m-%d")
+        print("maq: " + data + ", Stock carregado, Estado atualizado.")
+        print("maq: Bom dia. Estou disponível para atender o seu pedido.")
+        while True:
+            comando = input(">> ").strip()
+            comando_lista = comando.split()
+
+            match comando_lista[0]:
+                case "LISTAR":
+                    maquina.listar()
+                
+                case "MOEDA":
+                    moedas = comando[6:].replace(".", "").split(",")  # Remove o ponto final e separa moedas
+                    moedas = [m.strip() for m in moedas]  # Remove espaços extras
+                    maquina.inserir_moedas(moedas)
+                    saldo = maquina.verificar_saldo()
+                    print(f"maq: Saldo = {int(saldo)}e{int((saldo * 100) % 100)}c")
+                
+                case "SAIR":
+                    troco = maquina.devolver_troco()
+                    if troco:
+                        print("maq: Pode retirar o troco:", troco)
+                    print("maq: Até à próxima!")
+                    break
+                
+                case _:
+                    print("maq: Comando inválido. Tente novamente.")
+
     
 
 
