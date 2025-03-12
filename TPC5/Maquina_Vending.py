057790920132ff22d493993733d8b691263184d0
Jose Vasconcelos <zevasconcelos6@gmail.com>
2025-03-12 12:38:01 +0000

    tpc5

--- TPC5/Maquina_Vending.py
+++ TPC5/Maquina_Vending.py
@@ -60,21 +60,20 @@ class Maquina_Vending:
         return ", ".join(f"{qtd}x {moeda}" for moeda, qtd in resultado_troco.items())
     
     def selecionar_produto(self, codigo_produto):
-            produto = None
-            for item in self.stock:
-                if item['cod'] == codigo_produto:
-                    produto = item
-            
-            if produto is None:
-                print("maq: Código inválido.")
-                return
-            
-            if self.saldo < produto['preco']:
-                print("maq: Saldo insuficiente para satisfazer o seu pedido")  
-                print(f"maq: Saldo = {int(self.saldo * 100)}c}; Pedido = {produto['preco']}")
-            elif produto['quant'] > 0:
-                self.saldo -= produto['preco']
-                print(f"maq: Pode retirar o produto dispensado \"{produto['nome']}\"")
-                print(f"maq: Saldo = {int(self.saldo * 100)}c")
-                produto['quant'] -= 1
-   
+        produto = None
+        for item in self.stock:
+            if item['cod'] == codigo_produto:
+                produto = item
+        
+        if produto is None:
+            print("maq: Código inválido.")
+            return
+        
+        if self.saldo < produto['preco']:
+            print("maq: Saldo insuficiente para satisfazer o seu pedido")  
+            print(f"maq: Saldo = {self.saldo}; Pedido = {produto['preco']}")
+        elif produto['quant'] > 0:
+            self.saldo -= produto['preco']
+            print(f"maq: Pode retirar o produto dispensado \"{produto['nome']}\"")
+            print(f"maq: Saldo = {self.saldo}")
+            produto['quant'] -= 1
--- TPC5/README.md
+++ TPC5/README.md
@@ -10,12 +10,52 @@ Número Mecanográfico: A100763 <br> <br> <br>
 <br>
 
 ## **Resumo**
+Programa escrito em python, que simula uma máquina de vending.
+
+Inicialmente, é carregado um ficheiro JSON que listas de triplos,
+compostas pelo nome do produto, quantidade e preço. (ficheiro de exemplo [stock.json](stock.json))
 
 
+## **Comandos**
+
+### AJUDA
+```
+>> AJUDA
+```
+
+### LISTAR
+Exemplo:
+```
+>> LISTAR
+```
+
+
+### SELECIONAR
+Exemplo:
+```
+>> SELECIONAR A23
+
+```
+
+
+### MOEDA
+Exemplo:
+```
+>> MOEDA 1e, 20c, 5c, 5c .
+maq: Saldo = 1e30c
+
+```
+
+### SAIR
+Exemplo:
+```
+>> SAIR
+maq: Pode retirar o troco: 1x 50c, 1x 20c e 2x 2c.
+maq: Até à próxima
+```
 
 
 ## **Resultados**
 
-**Ficheiro de teste:** [query.txt](./query.txt)
-<br>
+
 
